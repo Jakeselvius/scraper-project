b254c11bd9df7ff694b29d401342befbc75dc238
Jake Selvius <jakeselvius@gmail.com>
2026-03-09 18:05:29 -0400

    got individual book data and cleaned it up the price & rating

--- scraper.py
+++ scraper.py
@@ -7,8 +7,8 @@ from bs4 import BeautifulSoup
 # prints off their values and data
 url = "https://books.toscrape.com" 
 response = requests.get(url) 
-print(f"Status code = {response.status_code}") # hould be 200 if succesufl
-print(response.text[:500]) # prints the first 500 characters of HTML
+#print(f"Status code = {response.status_code}") # hould be 200 if succesufl
+#print(response.text[:500]) # prints the first 500 characters of HTML
 
 """========================================================================"""
 
@@ -30,9 +30,32 @@ url = "https://books.toscrape.com"
 response = requests.get(url)
 soup = BeautifulSoup(response.text, "html.parser")
 books = soup.find_all("article", class_="product_pod")
-print(f"Found {len(books)} books on this page") # should print 20
+#print(soup.prettify())
+#print(f"Found {len(books)} books on this page") # should print 20
 
 """========================================================================"""
 
 
+# MAKING THE DATA LOOK NICE
+def clean_price(price_text):
+    return float(price_text.replace("Â£", ""))
+
+def clean_rating(rating_word):
+    rating_map = {"One": 1, "Two": 2, "Three": 3, "Four": 4, "Five": 5}
+    return rating_map.get(rating_word, 0)
+
+"""========================================================================"""
+
+
+# GETTING DATA FROM EACH INDIVIDUAL BOOK
+# 'title' finds heading 'h3', then looks at 'a' where words = 'title'
+# 'price_text' finds paragraph 'p' where class name = 'price color' and grabs the text
+# rating_word finds paragraph 'p' where class name = 'star-rating' and grabs the last word of that
+for book in books:
+    title = book.h3.a['title']
+    price_text = book.find('p', class_='price_color').text
+    rating_word = book.find('p', class_='star-rating')['class'][1]
+    print(f'{title} | {clean_price(price_text)} | {clean_rating(rating_word)}')
+   
+"""========================================================================"""
 

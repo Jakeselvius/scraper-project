9ca2d1c7c96b73391d87087a7a07f2ff126805ed
Jake Selvius <jakeselvius@gmail.com>
2026-03-09 18:36:16 -0400

    Cleaned up the code a little bit. commented out my earler steps and left my end result

--- scraper.py
+++ scraper.py
@@ -6,8 +6,8 @@ import time
 # 'url' assigns the website we are looking at
 # 'response' pulls the info from that website
 # prints off their values and data
-url = "https://books.toscrape.com" 
-response = requests.get(url) 
+# url = "https://books.toscrape.com" 
+# response = requests.get(url) 
 #print(f"Status code = {response.status_code}") # hould be 200 if succesufl
 #print(response.text[:500]) # prints the first 500 characters of HTML
 
@@ -18,7 +18,7 @@ response = requests.get(url)
 
 # FINDING WHAT HTML TO LOOK FOR & PARSING HTML USING BeautifulSoup
 # 'soup' looks through the HTML
-# 'books' finds all book containers (each one has a <article> tag)
+# 'books' finds all books (each one has a <article> tag)
 """<article class="product_pod">
   <h3>
     <a href="..." title="A Light in the Attic">A Light in the Attic</a>
@@ -27,13 +27,26 @@ response = requests.get(url)
   <p class="star-rating Three"></p>
 </article>
 """
-url = "https://books.toscrape.com"
-response = requests.get(url)
-soup = BeautifulSoup(response.text, "html.parser")
-books = soup.find_all("article", class_="product_pod")
+# url = "https://books.toscrape.com"
+# response = requests.get(url)
+# soup = BeautifulSoup(response.text, "html.parser")
+# books = soup.find_all("article", class_="product_pod")
 #print(soup.prettify())
 #print(f"Found {len(books)} books on this page") # should print 20
 
+"""========================================================================"""
+
+
+# GETTING DATA FROM EACH INDIVIDUAL BOOK
+# 'title' finds heading 'h3', then looks at 'a' where words = 'title'
+# 'price_text' finds paragraph 'p' where class name = 'price color' and grabs the text
+# rating_word finds paragraph 'p' where class name = 'star-rating' and grabs the last word of that
+# for book in books:
+#     title = book.h3.a['title']
+#     price_text = book.find('p', class_='price_color').text
+#     rating_word = book.find('p', class_='star-rating')['class'][1]
+#     print(f'{title} | {clean_price(price_text)} | {clean_rating(rating_word)} |')
+   
 """========================================================================"""
 
 
@@ -44,22 +57,6 @@ def clean_price(price_text):
 def clean_rating(rating_word):
     rating_map = {"One": 1, "Two": 2, "Three": 3, "Four": 4, "Five": 5}
     return rating_map.get(rating_word)
-
-"""========================================================================"""
-
-
-# GETTING DATA FROM EACH INDIVIDUAL BOOK
-# 'title' finds heading 'h3', then looks at 'a' where words = 'title'
-# 'price_text' finds paragraph 'p' where class name = 'price color' and grabs the text
-# rating_word finds paragraph 'p' where class name = 'star-rating' and grabs the last word of that
-for book in books:
-    title = book.h3.a['title']
-    price_text = book.find('p', class_='price_color').text
-    rating_word = book.find('p', class_='star-rating')['class'][1]
-    print(f'{title} | {clean_price(price_text)} | {clean_rating(rating_word)} |')
-   
-"""========================================================================"""
-
 
 # FUNCTION TO SCRAPE A PAGE. ALLOWS US TO SCRAPE MULTIPLE PAGES
 def scrape_page(url):
@@ -79,7 +76,7 @@ def scrape_page(url):
 
 
 all_books = []
-for page_number in range(1, 51):
+for page_number in range(1, 6):
     if page_number == 1:
         url = "https://books.toscrape.com"
     else:

7a0bd53c6115fe5cd11f76b679ad49dceab0423e
Jake Selvius <jakeselvius@gmail.com>
2026-03-10 16:41:55 -0400

    Learned how to display the db using Pandas

---
+++ analysis.py
@@ -0,0 +1,58 @@
+import sqlite3
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# load our book database into the pandas dataframe
+connection = sqlite3.connect("books.db")
+df = pd.read_sql_query("SELECT * FROM BOOKS", connection)
+connection.close()
+
+# displaying the data
+"""print(df.head()) # the first 5 rows if not specified how many
+print(df.shape) # this prints out (number of rows, number of columns)
+print(df.describe()) # prints off mathmatical facts about the data
+print(df.dtypes) # this prints off the data type for each row"""
+
+"""============================================================"""
+
+# messing around with queries using some core Pandas Methods
+
+# count all occurrences of '____' with .value_counts
+print(f"Number of specific star ratings:\n{df['rating'].value_counts()}")
+
+# Aggregate queries using Pandas groupby() method
+avg_by_rating = df.groupby('rating')['price'].mean()
+print(f"\nAverage price by rating:{avg_by_rating}\n")
+
+# summary of multiple aggregate functions at once
+sumamry = df.groupby('rating').agg(
+    total_books = ('title', 'count'),
+    avg_price = ('price', 'mean'),
+    max_price = ('price', 'max')
+)
+print(sumamry)
+
+
+# sorting values by column with sort_values()
+
+#cheapest books first
+cheapest = df.sort_values('price').head(10)
+print(f"\n========== Top 10 Cheapest books: =========== \n{cheapest[['title', 'price', 'rating']]}")
+
+#most expensive books first
+most_expensive = df.sort_values('price', ascending=False).head(10)
+print(f"\n========== Top 10 Most Expensive books: =========== \n{most_expensive[['title', 'price', 'rating']]}")
+
+# Filering rows with conditions
+
+#books with 5 star rating
+five_stars = df[df['rating'] == 5] # need nested otherwise it returns True or False
+print(f"\n\n========== Books with 5-star ratings: ==========\n {five_stars}")
+
+#books under £10 
+cheap = df[df['price'] < 15.0]
+print(f"\n\n========== Books Under £15: ==========\n {cheap}")
+
+# combining conditional statements
+affordable_good_book = df[(df['price'] < 15.0) & (df['rating'] >= 4)]
+print(f"\n\n========== Books Under £15 with at least a 4 star rating: ==========\n {affordable_good_book}")
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,5 +1,1 @@
-print(f"Total books collected: {len(all_books)}")
-for book in all_books:
-    print("================================================================\n")
-    print(f"Title: {book["title"]} | Price: {book["price"]} | Rating: {book['rating']}\n")
-print("================================================================\n")
+title
